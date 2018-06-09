327759e60b2356d29dcf9ce67a838bbee6712b26
Moritz Zeman <momo@zeman.de>
2018-06-09 08:16:56 +0200

    last searched

--- Veranstaltungen iOS/FirstViewController.swift
+++ Veranstaltungen iOS/FirstViewController.swift
@@ -21,7 +21,9 @@ extension UIViewController {
 
 import UIKit
 
-class FirstViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class FirstViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITableViewDelegate, UITableViewDataSource {
+    
+    let defaultValues = UserDefaults.standard
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -59,6 +61,18 @@ class FirstViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         myVC.preis2passed = textPreis2.text!
         myVC.kategoriepassed = textKategorie.text!
         myVC.umkreispassed = textUmkreis.text!
+        if defaultValues.integer(forKey: "testzahl") != nil {
+            print("TestSerach")
+            print(defaultValues.integer(forKey: "testzahl"))
+            var i = defaultValues.integer(forKey: "testzahl")
+            let iString = String(i)
+            i = i + 1
+            defaultValues.set(i, forKey: "testzahl")
+            let DefaultString = "Array" + iString
+            let array : [String: String] = ["Preis1": textPreis1.text!, "Preis2": textPreis2.text!, "Kategorie": textKategorie.text!, "Umkreis": textUmkreis.text!]
+            print ("TestSearch: " + array["Preis1"]!)
+            defaultValues.set(array,forKey: DefaultString)
+        }
         navigationController?.pushViewController(myVC, animated: true)
     }
     func setDoneOnKeyboard() {
@@ -79,6 +93,7 @@ class FirstViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
     
     @IBOutlet weak var showcase: UIImageView!
     
+    @IBOutlet weak var searchedTableView: UITableView!
     let kategoriePickerData = [String](arrayLiteral: "Party", "Natur", "Familie", "Jugend")
     let umkreisPickerData = [String](arrayLiteral: "10km", "30km", "50km", "100km", ">100km")
     @IBOutlet weak var textKategorie: UITextField!
@@ -102,14 +117,66 @@ class FirstViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         
         setDoneOnKeyboard()
         
+        self.searchedTableView.delegate = self
+        self.searchedTableView.dataSource = self
+        
+        searchedTableView.reloadData()
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        searchedTableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of feed items
+        print(defaultValues.integer(forKey: "testzahl"))
+       return defaultValues.integer(forKey: "testzahl")
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Retrieve cell
+        print("test")
+        let cellIdentifier: String = "searchedCell"
+        let myCell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)!
+        // Get the location to be shown
+        let countString = String(indexPath.row)
+        let arraystring = "Array" + countString
+        print(arraystring)
+        let readArray = defaultValues.value(forKey: arraystring)! as! NSDictionary
+        let kategorie = readArray["Kategorie"] as? String
+        let Umkreis = readArray["Umkreis"] as? String
+        let Preis1 = readArray["Preis1"] as? String
+        let Preis2 = readArray["Preis2"] as? String
+        print("test: " + Preis2!)
+        let itemData = kategorie! + ", " + Umkreis! + ", " + Preis1! + "€ bis " + Preis2! + "€"
+        myCell.textLabel!.text = itemData
+        
+        return myCell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let countString = String(indexPath.row)
+        let arraystring = "Array" + countString
+        let readArray = defaultValues.value(forKey: arraystring)! as! NSDictionary
+        let myVC = storyboard?.instantiateViewController(withIdentifier: "TableView") as! TableViewController
+        myVC.preis1passed = (readArray["Preis1"] as? String)!
+        myVC.preis2passed = (readArray["Preis2"] as? String)!
+        myVC.kategoriepassed = (readArray["Kategorie"] as? String)!
+        myVC.umkreispassed = (readArray["Umkreis"] as? String)!
+        navigationController?.pushViewController(myVC, animated: true)
+    }
+    }
 
-}
+
 
